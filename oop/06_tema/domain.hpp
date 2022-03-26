aee3bedc1cad198053167b3a917a30fd00582bca
Jardan Andrei <jardan.andrei@gmail.com>
2022-03-26 11:27:39 +0200

    POO tema 6

---
+++ oop/06_tema/domain.hpp
@@ -0,0 +1,74 @@
+#ifndef domain_h_INCLUDED
+#define domain_h_INCLUDED
+
+#include <ostream>
+#include <string>
+
+class Carte {
+private:
+	int id;
+	std::string titlu, autor, gen;
+	int an;
+public:
+	// Constructor pentru carte.
+	// in: id -- id-ul cartii construite
+	//     titlu -- titlul cartii construite
+	//     autor -- autorul cartii construite
+	//     gen -- genul cartii construite
+	//     an -- anul in care a fost publicata cartea construita
+	Carte(
+		int id,
+		std::string titlu,
+		std::string autor,
+		std::string gen,
+		int an
+	);
+	
+	// Getter pentru id-ul cartii.
+	// out: titlul id-ul
+	int get_id() const;
+	
+	// Getter pentru titlul cartii.
+	// out: titlul cartii
+	std::string get_titlu() const;
+	
+	// Getter pentru autorul cartii.
+	// out: autorul cartii
+	std::string get_autor() const;
+	
+	// Getter pentru genul cartii.
+	// out: genul cartii
+	std::string get_gen() const;
+	
+	// Getter pentru anul cartii.
+	// out: anul cartii
+	int get_an() const;
+	
+	// Setter pentru titlul cartii.
+	// in: titlu -- titlul nou
+	void set_titlu(std::string titlu);
+	
+	// Setter pentru autorul cartii.
+	// in: autor -- autorul nou
+	void set_autor(std::string autor);
+	
+	// Setter pentru genul cartii.
+	// in: gen -- genul nou
+	void set_gen(std::string gen);
+	
+	// Setter pentru anul cartii.
+	// in: an -- anul nou
+	void set_an(int an);
+	
+	friend bool operator==(const Carte &lhs, const Carte &rhs);
+	
+	friend std::ostream &operator<<(std::ostream &out, const Carte &carte);
+};
+
+bool operator==(const Carte &lhs, const Carte &rhs);
+std::ostream &operator<<(std::ostream &out, const Carte &carte);
+
+void test_domain();
+
+#endif // domain_hpp_INCLUDED
+
---
+++ oop/06_tema/repo.hpp
@@ -0,0 +1,58 @@
+#ifndef repo_hpp_INCLUDED
+#define repo_hpp_INCLUDED
+
+#include <vector>
+
+#include "./domain.hpp"
+
+class RepoCartiException {
+private:
+	std::string msg;
+public:
+	RepoCartiException(std::string msg);
+};
+
+class RepoCarti {
+private:
+	std::vector<Carte> carti;
+	
+	// Functie interna care cauta o carte in vector dupa id.
+	// in: id -- id-ul dupa care se cauta cartea
+	// out: indicele cartii in vectorul de carti, sau -1 daca cartea nu exista
+	int get_id_index(int id) const;
+public:
+	// Creeaza un nou repo de carti.
+	RepoCarti() = default;
+	
+	// Nu permitem sa se copieze un repo de carti.
+	RepoCarti(const RepoCarti &repo_carti) = delete;
+	
+	// Adauga o carte in repo.
+	// in: carte -- cartea care va fi adaugata
+	// raise: RepoCartiException, daca cartea care se adauga este deja in repo
+	void add(Carte carte);
+	
+	// Sterge o carte din repo.
+	// in: id -- id-ul cartii care va fi stearsa
+	// raise: RepoCartiException, daca cartea cu id-ul furnizat nu exista in repo
+	void erase(int id);
+	
+	// Actualizeaza valoarea unei carti in repo.
+	// in: carte -- cartea care va fi actualizata
+	// raise: RepoCartiException, daca cartea nu exista in repo
+	void update(Carte carte);
+	
+	// Cauta cartea cu id-ul furnizat in repo.
+	// in: id -- id-ul cartii care este cautata
+	// out: cartea cu id-ul furnizat
+	// raise: RepoCartiException, daca cartea cu id-ul furnizat nu a putut fi gasita
+	Carte find(int id) const;
+	
+	// Returneaza toate cartile din repo.
+	const std::vector<Carte> &get_all() const;
+};
+
+void test_repo();
+
+#endif // repo_hpp_INCLUDED
+
