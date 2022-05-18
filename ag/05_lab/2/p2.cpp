f808be21506ab82236c98d285f3de1f74fe6f752
Jardan Andrei <jardan.andrei@gmail.com>
2022-05-18 16:56:16 +0300

    AG Tema 5

--- ag/05_lab/2/p2.cpp
+++ ag/05_lab/2/p2.cpp
@@ -11,9 +11,9 @@ const int E = 50041;
 int v, e;
 
 int src, dst;
-int f[V][V];
+int r[V][V];
 int h[V];
-int ex[V];
+int excess[V];
 vector<int> gra[V];
 
 int main(int argc, char **argv) {
@@ -31,55 +31,49 @@ int main(int argc, char **argv) {
 		gra[a].push_back(b);
 		gra[b].push_back(a);
 		if (a == src) {
-			ex[a] -= c;
-			ex[b] += c;
-			f[b][a] += c;
+			excess[a] -= c;
+			excess[b] += c;
+			r[b][a] += c;
 		} else {
-			f[a][b] += c;
+			r[a][b] += c;
 		}
 	}
 	
-	int idk = 0;
+	int empty = 0;
 	int a = 1;
-	while (idk < v-2) {
-		int old_h = h[a];
-		int i = 0;
-		while (ex[a] > 0) {
-			cout << ex[a] << endl;
-			if (i < gra[a].size()) {
-				int b = gra[a][i];
-				if (f[a][b] > 0 && h[a] == h[b]+1) {
-					int c = min(ex[a], f[a][b]);
-					
-					// pompare()
-						ex[b] += c;
-						ex[a] -= c;
-						f[a][b] -= c;
-						f[b][a] += c;
-					//
-				}
-				i++;
-			} else {
-				// inaltare()
-					int mini = -1;
-					for (int j = 0; j < gra[a].size(); ++j) {
-						int b = gra[a][j];
-						if (f[a][b] > 0) {
-							if (mini == -1 || h[b] < mini) {
-								mini = b;
-							}
-						}
-					}
-					h[a] = mini+1;
-				//
-				i = 0;
-			}
+	while (empty < v-2) {
+		if (excess[a] == 0) {
+			empty++;
+		} else {
+			empty = 0;
 		}
 		
-		if (h[a] <= old_h) {
-			idk++;
-		} else {
-			idk = 0;
+		while (excess[a] > 0) {
+			for (int i = 0; excess[a] > 0 && i < gra[a].size(); ++i) {
+				int b = gra[a][i];
+				if (r[a][b] > 0 && h[a] == h[b]+1) {
+					// pompare()
+						int c = min(excess[a], r[a][b]);
+						excess[b] += c;
+						excess[a] -= c;
+						r[a][b] -= c;
+						r[b][a] += c;
+					//
+				}
+			}
+			
+			// inaltare()
+				int mini = -1;
+				for (int i = 0; i < gra[a].size(); ++i) {
+					int b = gra[a][i];
+					if (r[a][b] > 0) {
+						if (mini == -1 || h[b] < mini) {
+							mini = h[b];
+						}
+					}
+				}
+				h[a] = mini+1;
+			//
 		}
 		
 		a++;
@@ -88,7 +82,7 @@ int main(int argc, char **argv) {
 		}
 	}
 	
-	fout << ex[dst];
+	fout << excess[dst];
 	
 	return 0;
 }
