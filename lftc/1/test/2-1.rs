4b9815d1a3b2dcf00f03274f4cc874c425291661
Jardan Andrei <jardan.andrei@gmail.com>
2023-10-11 14:11:36 +0300

    lftc tema 1

---
+++ lftc/1/test/2-1.rs
@@ -0,0 +1,14 @@
+fn main() {
+  let mut input = String::new();
+  let stdin = std::io::stdin();
+
+  stdin.read_line(&mut input).unwrap();
+	let radius: f32 = input.trim().parse().unwrap();
+	input.clear();
+
+	let perimeter = std::f32::consts::PI * 2. * radius;
+	let area = std::f32::consts::PI * radius * radius;
+
+	println!("perimeter = {}", perimeter);
+	println!("area = {}", area);
+}
